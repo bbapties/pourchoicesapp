22b3d093b154bda4b0abdd246a7e2ac08c6aa0b3
whiskeypicks <whiskeypicksapp@gmail.com>
2025-11-22 20:15:24 -0500

    fix: add client global-error to fix prerender build

---
+++ src/app/global-error.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <html>
+      <body className="flex flex-col items-center justify-center h-screen bg-gray-100 gap-4">
+        <h2 className="text-2xl font-bold">Something went wrong!</h2>
+        <Button onClick={() => reset()} size="lg">
+          Try again
+        </Button>
+      </body>
+    </html>
+  );
+}
