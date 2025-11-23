751d77d1cd4038a67e77c16bc69709f98b70e2ff
whiskeypicks <whiskeypicksapp@gmail.com>
2025-11-22 22:23:54 -0500

    commit and deploy seperate

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-﻿/** @type {import('next').NextConfig} */
-const nextConfig = {
-  allowedDevOrigins: ['192.168.68.82'], // Your phone IP
-  experimental: {
-    turbo: false, // Disables Turbopack for builds to fix prerender errors in Next.js 16
-  },
-};
-
-export default nextConfig;
